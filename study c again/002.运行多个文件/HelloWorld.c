8b5165230d58a0b2b2623ab60200a7a64210f2da
isikveren <lauxunzi@outlook.com>
2021-05-25 22:51:11 +0800

    update

---
+++ study c again/002.运行多个文件/HelloWorld.c
@@ -0,0 +1,9 @@
+#include "print.h"
+
+int main(){
+    printHello();
+    return 0;
+}
+
+// 终端输入：gcc HelloWorld.c print.c -o output.out
+// 运行output.out
---
+++ study c again/README.md
@@ -0,0 +1,5 @@
+<h1>重新学习c语言</h1>
+
+<p>根据：
+<a href = "https://github.com/Mzzopublic/C/tree/master/经典示例">github</a>
+</p>
